5ad56dd21b78e6551935d35b0518debdde832a70
ngcwzy <x00217230235@njit.edu.cn>
2024-03-04 22:34:06 +0800

    bubble

---
+++ bubbleSort/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception{
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修复。
+        int[] array = {38,65,3,17,2};
+
+        App app = new App();
+        app.App(array);
+    }
+}
